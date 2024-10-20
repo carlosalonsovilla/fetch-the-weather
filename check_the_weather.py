323d300a0e34b66594d95c896fa74464ba6e6955
carlosalonsovilla <cav2589@gmail.com>
2024-10-20 14:44:25 -0400

    Allow user to enter the city they want to check the weather for.

--- check_the_weather.py
+++ check_the_weather.py
@@ -1,12 +1,17 @@
 import requests
 import os
 from dotenv import load_dotenv
-import json
-
 
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
+
+city = input("Enter the city you want to check the weather for: ").capitalize()
+country = input(f"What country is {city} in? ").upper()
+
+if country == 'US':
+    state = input(f"What state is {city} in? (e.g., Florida, New York)? ").capitalize()
+    city = f"{city},{state}"
 
 def get_weather(city):
     url = 'http://api.openweathermap.org/data/2.5/weather'
@@ -19,11 +24,15 @@ def get_weather(city):
     response = requests.get(url, params=params)
     if response.status_code == 200:
         data = response.json()
-        pretty = json.dumps(data, indent=4)
-        print(pretty)
+        temp = data['main']['temp']
+        
+        if country == 'US':
+            print(f"The current temperature in {city}, {country} is {temp}°F")
+        else:
+            print(f"The current temperature in {city.split(' ,')[0]}, {country.capitalize()} is {temp}°F")
+
     else:
         print(f"Failed to get the weather. Status code: {response.status_code}")
 
 if __name__ == '__main__':
-    city = 'St Petersburg,us'
     get_weather(city)
